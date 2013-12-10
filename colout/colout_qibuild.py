9141caad08f4480b25364965e49ade905468f824
Louis-Kenzo Cahier <lkcahier@aldebaran-robotics.com>
2013-12-10 16:32:54 +0100

    Small adjustments to qi themes; adjusted colors to typical ANSI colors

--- colout/colout_qibuild.py
+++ colout/colout_qibuild.py
@@ -1,74 +1,60 @@
 
 def theme():
-    # CMake theme:
-    #  actions performing in cyan
-    performing="cyan"
-    #  actions performed in green
-    performed="green"
-    #  actions taking an unknown time
-    untimed="blue"
+    neutral="white"
+    informative="cyan"     # e.g. 117
+    choice="magenta"       # e.g. 135
+    error="red"            # e.g. 197
+    warning="yellow"       # e.g. 214
+    section_label="yellow" # e.g. 214
+    section_title="green"  # e.g. 154
+    second_class_object="blue" # e.g. 230
+    first_class_object="green" # e.g. 154
 
     return [
-	# qibuild configure
-	[ "^(Current build worktree:) (.*/)([^/]+)$", "214,154,154", "normal,normal,bold" ],
-        [ "^(Using toolchain:) (.*)$", "214,154", "normal,bold" ],
-        [ "^(Build type:) (.*)$", "214,154", "normal,bold" ],
-        [ "^(\* \( *[0-9]+/[0-9]+\)) (Configuring) (.*)", "197,214,154", "bold,normal,bold" ],
+        # qibuild configure
+        [ "^(Current build worktree:) (.*/)([^/]+)$", ",".join([section_label,section_title,section_title]), "normal,normal,bold" ],
+        [ "^(Using toolchain:) (.*)$", ",".join([section_label,section_title]), "normal,bold" ],
+        [ "^(Build type:) (.*)$", ",".join([section_label,section_title]), "normal,bold" ],
+        [ "^(\* \( *[0-9]+/[0-9]+\)) (Configuring) (.*)", ",".join(["red", section_label,section_title]), "bold,bold,bold" ],
         # qibuild warning
-        [ "^(\[WARN \]:) (.*)", "red", "normal,bold" ],
-        [ "^-- Library: (.*)$", "yellow", "bold" ],
-        [ "^-- Binary: (.*)$", "154", "bold" ],
-        # Configure
-        [ "^-- (The) (.* compiler) (identification is) (.*)$", "117", "normal,bold,normal,bold" ],
-        [ "^-- (Check for working) ([^ ]+ compiler): (.*/)([^/]+)( -- works)$", "117", "normal,bold,normal,bold" ],
-        [ "^-- (Detecting) ([^ ]+ compiler ABI info - done)$", "117", "normal,bold" ],
-        [ "^-- (Looking for include files) ([^ ]+) (- found)$", "117", "normal,bold" ],
-        [ "^-- (Looking for include files) ([^ ]+) (- not found)$", "red", "normal,bold" ],
-        [ "^-- (Looking for) ([^ ]+) (in) (.*) (- found)$", "117", "normal,bold,normal,normal,bold" ],
-        [ "^-- (Looking for) ([^ ]+) (in) (.*) (- not found)$", "red", "normal,bold,normal,normal,bold" ],
-        [ "^-- (Looking for) ([^ ]+) (- found)$", "117", "normal,bold" ],
-        [ "^-- (Looking for) ([^ ]+) (- not found.)$", "red", "normal,bold" ],
-        [ "^-- (Configuring done)$", "117", "bold" ],
-        [ "^-- (Generating done)$", "117", "bold" ],
-        [ "^-- (Build files have been written to:) (.*/)([^/]+)$", "135", "bold,normal,bold" ],
-        [ "^-- (Using) (.*)$", "135", "normal,bold" ],
-        [ "^-- (Build Options:)$", "135", "bold" ],
-        [ "^-- (WITH_.* : .*)$", "135", "bold" ],
-        [ "^-- (Found) (.*): (.*) (\(found version) (.*)(\))$", "117", "normal,bold,bold,normal,bold,normal" ],
-        # Build
-        [ "^(\* \( *[0-9]+/[0-9]+\)) (Building) (.*)", "197,214,154", "bold,normal,bold" ],
-        [ "^(Scanning dependencies of target) (.*)$", "white", "normal,bold" ],
-        [ "^\[\s*[0-9]+%\]\s(Building \w* object)(\s+.*/)([-\w]+.c.*)(.o)$", "117", "normal,normal,bold,bold"],
-        [ "^\[\s*[0-9]+%\]\s(Built target)(\s.*)$", "214,154", "normal,bold" ],
-        [ "^(Linking .*) (shared library|library|executable) (.*/)*(.+(\.[aso]+)*)$", "white", "normal,normal,normal,bold,bold" ],
-        [ "^\[\s*[0-9]+%\]\s(Generating)(\s+.*)$", "white", "normal" ],
-        # 
-        [ "^-- Found.*NO", "red" ],
-        [ "^--.*broken", "red" ],
-        [ "^-- Coult NOT find.*", "red" ],
-        [ "^-- Configuring incomplete, errors occurred!", "red" ],
-        #[ "^--.*", performing ],
-        # Errors
-        [ "CMake Error:", "red" ],
-        [ "CMake Warning", "yellow" ],
-        # Scan
-        #[ "^(Scanning dependencies of target)(.*)$",
-        #  performing, "normal,bold" ],
-        # Link
-        #[ "^(Linking .* )(library|executable) (.*/)*(.+(\.[aso]+)*)$",
-        #  untimed, "normal,normal,bold" ],
-        # [percent] Built
-        #[ "^\[\s*[0-9]+%\]\s(Built target)(\s.*)$",
-        #  performed, "normal,bold" ],
-        # [percent] Building
-        #[ "^\[\s*[0-9]+%\]\s(Building \w* object)(\s+.*/)([-\w]+.c.*)(.o)$",
-        #    performing, "normal,normal,bold,normal"],
-        # [percent] Generating
-        #[ "^\[\s*[0-9]+%\]\s(Generating)(\s+.*)$",
-        #    performing, "normal,bold"],
+        [ "^(\[WARN \]:) (.*)", error, "normal,bold" ],
+        [ "^-- Library: (.*)$", second_class_object, "bold" ],
+        [ "^-- Binary: (.*)$", first_class_object, "bold" ],
+        # CMake configure
+        [ "^-- (The) (.* compiler) (identification is) (.*)$", ",".join([neutral,neutral,neutral,informative]), "normal,bold,normal,bold" ],
+        [ "^-- (Check for working) ([^ ]+ compiler): (.*/)([^/]+)( -- works)$", ",".join([neutral,neutral,neutral,informative]), "normal,bold,normal,bold" ],
+        [ "^-- (Detecting) ([^ ]+ compiler ABI info - done)$", neutral, "normal,bold" ],
+        [ "^-- (Looking for include files?) ([^ ]+) (- found)$", ",".join([neutral,informative,informative]), "normal,bold,bold" ],
+        [ "^-- (Looking for include files?) ([^ ]+) (- not found)$", error, "normal,bold" ],
+        [ "^-- (Looking for) ([^ ]+) (in) (.*) (- found)$", ",".join([neutral,informative,neutral,neutral,informative]), "normal,bold,normal,normal,bold" ],
+        [ "^-- (Looking for) ([^ ]+) (in) (.*) (- not found)$", error, "normal,bold,normal,normal,bold" ],
+        [ "^-- (Looking for) ([^ ]+) (- found)$", ",".join([neutral,neutral,neutral]), "normal,bold,bold" ],
+        [ "^-- (Looking for) ([^ ]+) (- not found)$", error, "normal,bold" ],
+        [ "^-- (Configuring done)$", informative, "bold" ],
+        [ "^-- (Generating done)$", informative, "bold" ],
+        [ "^-- (Build files have been written to:) (.*/)([^/]+)$", choice, "bold,normal,bold" ],
+        [ "^-- (Using) (.*)$", choice, "normal,bold" ],
+        [ "^-- (No) (.*) (library found,) (.*) (won't be built.)$", error, "normal,bold,normal,bold,normal" ],
+        [ "^-- (Build Options:)$", choice, "bold" ],
+        [ "^-- (WITH_.* : .*)$", choice, "bold" ],
+        [ "^-- (Found) (.*): (.*) (\(found version) (.*)(\))$", informative, "normal,bold,bold,normal,bold,normal" ],
+        # CMake build
+        [ "^(\* \( *[0-9]+/[0-9]+\)) (Building) (.*)", ",".join(["red", section_label,section_title]), "bold,normal,bold" ],
+        [ "^(Scanning dependencies of target) (.*)$", neutral, "normal,bold" ],
+        [ "^\[\s*[0-9]+%\]\s(Building \w* object)(\s+.*/)([-\w]+.c.*)(.o)$", informative, "normal,normal,bold,bold"],
+        [ "^\[\s*[0-9]+%\]\s(Built target)(\s.*)$", ",".join([section_label,section_title]), "normal,bold" ],
+        [ "^(Linking .*) (shared library|library|executable) (.*/)*(.+(\.[aso]+)*)$", neutral, "normal,normal,normal,bold,bold" ],
+        [ "^\[\s*[0-9]+%\]\s(Generating)(\s+.*)$", neutral, "normal" ],
+        # CMake errors and warnings
+        [ "^-- Found.*NO", error ],
+        [ "^--.*broken", error ],
+        [ "^-- Coult NOT find.*", error ],
+        [ "^-- Configuring incomplete, errors occurred!", error ],
+        [ "CMake Error:", error, "reverse" ],
+        [ "CMake Warning", warning, "reverse" ],
         # make errors
         [ "make\[[0-9]+\].*", "yellow"],
-        [ "(make: \*\*\* \[.+\] )(.* [0-9]+)", "red", "normal,bold"],
-        # progress percentage
+        [ "(make: \*\*\* \[.+\] )(.* [0-9]+)", error, "normal,bold"],
+        # Progress percentage
         [ "^(\[\s*[0-9]+%\])","Scale" ]
     ]
