ef4b1c862e1e4a5fe62bcfd42a5ff161ea07170c
Louis-Kenzo Cahier <lkcahier@aldebaran-robotics.com>
2013-12-13 19:23:42 +0100

    Adjustments to qilog theme

--- colout/colout_qilog.py
+++ colout/colout_qilog.py
@@ -1,18 +1,29 @@
 
 def theme():
-    subdued = "black" # e.g. 242
-    neutral = "white" # e.g. 117
-    info    = "white" # e.g. 117
-    error   = "red"   # e.g. 197
-    warning = "blue"  # e.g. 214
+    subdued = "black"  # e.g. 242
+    neutral = "white"  # e.g. 117
+    info    = "cyan"   # e.g. 117
+    error   = "red"    # e.g. 197
+    warning = "yellow" # e.g. 214
+
+    numbering = "white"
+    absolute_filename = "magenta"
+    number = "yellow"
+    ip = "green"
+    time = "red"
 
     return [
-        [ "^(\[D\]) ", subdued, "bold" ],
-        [ "^(\[V\]) ", neutral, "bold" ],
-        [ "^(\[I\]) ", info, "bold" ],
+        [ " (?:[0-9]+\.[0-9]+ )?[0-9]+ ((?:[a-zA-Z]+[.])*[a-zA-Z]+): ", "hash" ],
+        [ "\] ([0-9]+\.[0-9]+ )?([0-9]+) ", subdued ],
+        [ "^(\[D\]) ", subdued ],
+        [ "^(\[V\]) ", neutral ],
+        [ "^(\[I\]) ", info ],
         [ "^(\[W\]) ", warning, "bold" ],
-        [ "^(\[E\]) ", error, "bold" ],
-        [ " (?:[0-9]+\.[0-9]+ )?[0-9]+ ((?:[a-zA-Z]+[.])*[a-zA-Z]+): ", "Hash", "bold" ],
-        [ " ([0-9]+\.[0-9]+ )?([0-9]+) ", subdued ],
-        [ " (Registered Service) \"([a-zA-Z0-9]+)\" \(#[0-9]+\)", neutral, "underline,bold" ]
+        [ "^(\[E\]) ", error, "reverse" ],
+        [ " Registered Service \"([a-zA-Z0-9]+)\" \(#[0-9]+\)", neutral, "bold" ],
+        [ "#[0-9]+", numbering ],
+        [ "(/(?:[^/]+/)+)([^/]+\.[a-zA-Z0-9]+)", absolute_filename, "normal,bold" ],
+        [ "\s[0-9]+[\s$]", number],
+        [ "(?:[a-z]+\://)([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(?:\:[0-9+])", ip, "bold"],
+        [ "([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}\:[0-9]{2})(\:[0-9]{2}) ([A-Z]+(?:[+-][0-9]{1,2})?)", time, "normal,bold,normal"]
     ]
